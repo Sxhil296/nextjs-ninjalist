0052dcfc8f53991ed36deaba441eb80a1a87f26c
Sahil <leviack629@gmail.com>
2023-02-13 16:22:05 +0530

    first

---
+++ components/Navbar.js
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <div className='logo'>
+            <h1>Ninja List</h1>
+        </div>
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+        <Link href="/ninjas">Ninjas</Link>
+        
+        
+        
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const about = () => {
+  return (
+    <div>
+       
+        <h1>AboutPage</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium odit excepturi illum tempora velit corporis molestias, consequuntur, labore fuga placeat vitae quod culpa iure fugit doloremque. Beatae ratione voluptas maiores!</p>
+      
+    </div>
+  )
+}
+
+export default about
